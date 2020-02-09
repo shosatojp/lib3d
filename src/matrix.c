9ecba2bbddd4cc2990059a7f66dd73afca0bffe3
sho <me@shosato.jp>
2020-02-09 12:34:19 +0900

    update

--- src/matrix.c
+++ src/matrix.c
@@ -21,6 +21,7 @@ void mul44(mat44 a, mat41 b, mat41 r) {
     for (int i = 0; i < 4; ++i) {
         for (int k = 0; k < 4; ++k) {
             r[i] += a[i + 4 * k] * b[k];
+            // printf("%f  =  %f  *  %f\n", r[i], a[i + 4 * k], b[k]);
         }
     }
 }
@@ -28,13 +29,13 @@ void mul44(mat44 a, mat41 b, mat41 r) {
 /**
  * a    4*4
  * b    4*4
- * r    4*1
+ * r    4*4
  * 
  * a != r, b != r
  * must initialize r : true
  * 
  */
-void mul4444(mat44 a, mat44 b, mat41 r) {
+void mul4444(mat44 a, mat44 b, mat44 r) {
     for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 4; ++j) {
             for (int k = 0; k < 4; ++k) {
@@ -109,8 +110,6 @@ void vector_product3(mat41 a, mat41 b, mat41 r) {
     r[3] = 1;
 }
 
-vtype unit_y[] = {0, 1, 0};
-
 vtype inner_product_1d(mat a, mat b, int n) {
     vtype sum = 0.0;
     for (int i = 0; i < n; ++i) {
@@ -119,27 +118,10 @@ vtype inner_product_1d(mat a, mat b, int n) {
     return sum;
 }
 
-// void view_axis(mat41 camera, mat41 target, mat41 up, mat41 cx, mat41 cy, mat41 cz) {
-//     // cz
-//     vector_sub(target, camera, cz, 3);
-//     normarize_vector(cz, cz, 3);
-//     cz[3] = 1;
-
-//     // cx
-//     vector_product3(&unit_y, cz, cx);
-//     normarize_vector(cx, cx, 3);
-//     cx[3] = 1;
-
-//     // cy
-//     vector_product3(cz, cx, cy);
-//     normarize_vector(cy, cy, 3);
-//     cy[3] = 1;
-// }
-
 /**
  * camera = target = upper = r
  */
-void world_to_camera_mat44(mat41 camera, mat41 target, mat41 upper, mat44 r) {
+void make_world_to_camera_mat44(mat41 camera, mat41 target, mat41 upper, mat44 r) {
     vtype tmp[4] = {0};
     vtype cx[4] = {0};
     vtype cy[4] = {0};
@@ -169,16 +151,16 @@ void world_to_camera_mat44(mat41 camera, mat41 target, mat41 upper, mat44 r) {
     r[15] = 1;
 }
 
-void camera_to_projection_mat44(vtype angle, vtype aspect, vtype near, vtype far, mat44 r) {
+void make_camera_to_projection_mat44(vtype angle, vtype aspect, vtype near, vtype far, mat44 r) {
     vtype tan_angle_2 = tan(angle / 2);
-    r[0] = 1 / tan_angle_2;
-    r[5] = 1 / (tan_angle_2 * aspect);
+    r[5] = 1 / tan_angle_2;
+    r[0] = r[5] / aspect;
     r[10] = far / (far - near);
     r[11] = 1;
     r[14] = -near * r[10];
 }
 
-void projection_to_screen_mat44(vtype width, vtype height, mat44 r) {
+void make_projection_to_screen_mat44(vtype width, vtype height, mat44 r) {
     vtype width_2 = width / 2.0;
     vtype height_2 = height / 2.0;
     r[0] = width_2;
@@ -235,60 +217,102 @@ void make_matrix_scale(vtype sx, vtype sy, vtype sz, mat44 r) {
     r[15] = 1;
 }
 
-int main() {
-    vtype a[] = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
-    // vtype b[] = {1, 2, 3, 1};
-    vtype r[4] = {0};
-    vtype r2[4] = {0};
-    int w = 400, h = 200;
-    // mul44(a, b, r);
+void mul44s(int c, mat44* as, mat44 r) {
+    if (c <= 1) {
+        fprintf(stderr, "invalid matrix count\n");
+    }
+    switch (c) {
+        case 2:
+            mul4444(as[1], as[0], r);
+        default: {
+            vtype tmp0[16] = {0};
+            vtype tmp1[16] = {0};
+            mul4444(as[c - 1], as[c - 2], tmp0);
+            int tmp_num = 0;
+            for (int i = c - 3; i > 0; i--) {
+                if (tmp_num == 0) {
+                    init_mat(tmp1, 4, 4);
+                    mul4444(tmp0, as[i], tmp1);
+                    tmp_num = 1;
+                } else {
+                    init_mat(tmp0, 4, 4);
+                    mul4444(tmp1, as[i], tmp0);
+                    tmp_num = 0;
+                }
+            }
+            if (tmp_num == 0) {
+                mul4444(tmp0, as[0], r);
+            } else {
+                mul4444(tmp1, as[0], r);
+            }
+        }
+    }
+}
 
-    vtype p_local[] = {10, 10, 10, 1};
-    vtype locals[] = {10, 10, 10, 1,
-                      10, 20, 10, 1,
-                      20, 20, 10, 1,
-                      20, 10, 10, 1,
-                      10, 10, 20, 1,
-                      10, 20, 20, 1,
-                      20, 20, 20, 1,
-                      20, 10, 20, 1
-                      };
-    vtype p_global[] = {0};
-
+void make_local_to_world_mat44(vtype dx, vtype dy, vtype dz,
+                               vtype sx, vtype sy, vtype sz,
+                               vtype theta_x, vtype theta_y, vtype theta_z, mat44 r) {
     vtype s[16] = {0};
+    vtype d[16] = {0};
     vtype rx[16] = {0};
     vtype ry[16] = {0};
     vtype rz[16] = {0};
+    make_matrix_transpose(-dx, -dy, -dz, d);
+    make_matrix_scale(sx, sy, sz, s);
+    make_matrix_round_x(-theta_x, rx);
+    make_matrix_round_y(-theta_y, ry);
+    make_matrix_round_z(-theta_z, rz);
 
-    // make_matrix_scale(2, 2, 4, s);
+    vtype* mat44s[] = {s, d, rz, ry, rx};
+    mul44s(5, (mat44*)mat44s, r);
+}
+
+int main() {
+    vtype r[4] = {0};
+    vtype r2[4] = {0};
+    int w = 400, h = 200;
+
+    vtype p_local[] = {10, 10, 10, 1};
+    vtype locals[] = {5, 5, 5, 1,
+                      5, -5, 5, 1,
+                      -5, -5, 5, 1,
+                      -5, 5, 5, 1,
+                      5, 5, -5, 1,
+                      5, -5, -5, 1,
+                      -5, -5, -5, 1,
+                      -5, 5, -5, 1};
+
     vtype camera[4] = {14, 20, -10, 1};
     vtype target[4] = {20, 20, 20, 1};
     vtype upper[4] = {0, 1, 0, 1};
+    vtype lw[16] = {0};
     vtype wc[16] = {0};
-    world_to_camera_mat44(camera, target, upper, wc);
     vtype cp[16] = {0};
-    camera_to_projection_mat44(90, 0.5, 10, 100, cp);
     vtype ps[16] = {0};
-    projection_to_screen_mat44(w, h, ps);
+    make_local_to_world_mat44(-15, -15, -15, 1, 1, 1, 0, 0, 0, lw);
+    make_world_to_camera_mat44(camera, target, upper, wc);
+    make_camera_to_projection_mat44(90, (double)w / h, 10, 100, cp);
+    make_projection_to_screen_mat44(w, h, ps);
+
+    vtype lp[16] = {0};
+    vtype* mat44s[] = {lw, wc, cp};
+    mul44s(3, (mat44*)mat44s, lp);
+    int white[3] = {0, 0, 0};
 
     unsigned char* buf = make_buffer(w, h, 255);
     for (int i = 0; i < 8; i++) {
         vtype* p_local = locals + i * 4;
-        init_mat(r2, 4, 1);
+
         init_mat(r, 4, 1);
-        mul44(wc, p_local, r2);
-        // mat_print(r2, 4, 1);
-        mat_print(r2, 4, 1);
-        mul44(cp, r2, r);
-        // mat_print(r, 4, 1);
+        mul44(lp, p_local, r);
 
         vector_div(r, r[3], r, 4);
-        // mat_print(r, 4, 1);
-        // mat_print(ps, 4, 4);
+
         init_mat(r2, 4, 1);
         mul44(ps, r, r2);
 
-        int white[3] = {0, 0, 0};
+        mat_print(r2, 4, 1);
+
         SET_BUFFER_RGB(buf, w, h, (int)r2[0], (int)r2[1], white);
     }
     write_buffer(buf, w, h, "hoge.ppm");
